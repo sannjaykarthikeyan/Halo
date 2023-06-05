9f9478dfb5b207e251709f20cf0565891d9b7b41
nointro3493 <95059950+nointro3493@users.noreply.github.com>
2023-06-05 14:47:48 -0400

    in case

--- app/src/main/java/com/example/halo/ui/learn/LearnFragment.java
+++ app/src/main/java/com/example/halo/ui/learn/LearnFragment.java
@@ -24,7 +24,7 @@ public class LearnFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
+        final TextView textView = binding.textView;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/com/example/halo/ui/learn/LearnViewModel.java
+++ app/src/main/java/com/example/halo/ui/learn/LearnViewModel.java
@@ -4,13 +4,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.halo.R;
+
 public class LearnViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
     public LearnViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is learn fragment");
+        // mText.setValue("Text");
     }
 
     public LiveData<String> getText() {
